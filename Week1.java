36b9248b6c9bd5b89c7133e0db8937bb76d4ab0c
Julio Chavez <jchav77@gmail.com>
2024-08-11 15:32:49 -0400

    Add initial project codebase

---
+++ Week1.java
@@ -0,0 +1,20 @@
+package javaProgrammingI;
+
+public class Week1 {
+
+	public static void main(String[] args) {
+		  	String FName = "First Name";
+	        String lName = "Last Name";
+	        String streetAddress = "Street Address";
+	        String city = "City";
+	        String zipCode = "Zip Code";
+
+	        System.out.println(FName);
+	        System.out.println(lName);
+	        System.out.println(streetAddress);
+	        System.out.println(streetAddress);
+	        System.out.println(zipCode);
+	}
+
+} 
+
